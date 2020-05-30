abd1fc0badc7475f3f2febcbab19e97b13497748
VisualDevelopment <drherocrafterslab@gmail.com>
2020-05-30 14:27:35 +0300

    feat: it's functional!

---
+++ config.py
@@ -0,0 +1,17 @@
+BUILD_TYPE = "Debug"
+OUTPUT_FILE = "Firework.img"
+SOURCES = [
+    {
+        "name": "Kernel",
+        "git": "https://github.com/Firework-OS/Kernel.git",
+        "cmd": f"make TARGET={BUILD_TYPE} OUTPUTDIR=../../build/Kernel -C ../sources/Kernel",
+        "files": [
+            {"src": "./Kernel/Kernel.bin", "dest": "/Firework/System/Kernel.bin"}
+        ]
+    },
+    {
+        "name": "qloader2",
+        "git": "https://github.com/qloader2/qloader2.git",
+        "cmd": "../sources/qloader2/qloader2-install ../sources/qloader2/qloader2.bin ./Firework.img"
+    },
+]
---
+++ fwstrap.py
@@ -0,0 +1,78 @@
+import config
+import subprocess
+from termcolor import colored
+import pathlib
+import os
+import traceback
+
+pathlib.Path("./build").mkdir(exist_ok=True)
+pathlib.Path("./sources").mkdir(exist_ok=True)
+
+output = f"./build/{config.OUTPUT_FILE}"
+
+if os.path.exists(output):
+    os.remove(output)
+
+print(f"\n{colored('==>', 'blue')} Creating image file:\n")
+subprocess.call(["dd", "if=/dev/zero", "bs=1M", "count=0",
+                 "seek=10", f"of={output}", "status=progress", "oflag=sync"])
+print(f"{colored('==>', 'blue')} Creating MBR.")
+subprocess.call(["parted", "-s", output, "mklabel", "msdos"])
+print(f"{colored('==>', 'blue')} Creating partition.")
+subprocess.call(["parted", "-s", output, "mkpart", "primary", "1", "100%"])
+print(f"{colored('==>', 'blue')} Formatting in format echfs.")
+subprocess.call(["echfs-utils", "-m", "-p0", output, "quick-format", "32768"])
+print(f"{colored('==>', 'blue')} Adding qloader2 config file.")
+subprocess.call(["echfs-utils", "-m", "-p0", output,
+                 "import", "./qloader2.cfg", "qloader2.cfg"])
+
+for source in config.SOURCES:
+    git = source["git"]
+    name = source["name"]
+
+    if not os.path.exists(f"./sources/{name}"):
+        print(f"\n{colored('==>', 'blue')} Cloning source '{name}'\n")
+        exit_code = subprocess.call(["git", "clone", git, f"./sources/{name}"])
+
+        if exit_code != 0:
+            print(f"\n{colored('E:', 'red')} Git exited with non-zero exit code.\n")
+            exit(exit_code)
+    else:
+        print(
+            f"\n{colored('==>', 'blue')} Pulling latest changes for source '{name}':\n")
+        os.chdir(f"./sources/{name}")
+        exit_code = subprocess.call(["git", "pull"])
+
+        if exit_code != 0:
+            print(
+                f"\n{colored('W:', 'yellow')} Git exited with non-zero exit code.\n")
+
+        os.chdir("../../")
+
+    os.chdir("./build")
+
+    cmd = source.get("cmd")
+
+    if cmd:
+        print(
+            f"\n{colored('==>', 'blue')} Calling build command for source '{name}':\n")
+        subprocess.call(cmd.split())
+    else:
+        print(f"\n{colored('W:', 'yellow')} No command for source '{name}'.\n")
+
+    files = source.get("files")
+
+    if files:
+        print(f"\n{colored('==>', 'blue')} Copying files for source '{name}':\n")
+
+        for file in files:
+            src = file["src"]
+            dest = file["dest"]
+
+            print(f"{src} {colored('==>', 'blue')} {dest}")
+            subprocess.call(["echfs-utils", "-m", "-p0",
+                             f"../{output}", "import", src, dest])
+
+    os.chdir("..")
+
+print(f"\n{colored('==>', 'green')} Done!\n")
